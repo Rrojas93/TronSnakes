61219e3955511c77410a5c10c844934e0f9d984f
Raul Rojas <rrojas@trentonsystems.com>
2021-03-10 02:18:56 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 import os
-
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.system("python3 -m pip install --upgrade pip")
 os.system("python3 -m pip install setuptools")
 os.system("python3 -m pip install -r requirements.txt")
+os.system("sudo chmod +x ./*.py")
